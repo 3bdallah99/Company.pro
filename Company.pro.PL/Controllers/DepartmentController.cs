065a5307d1727bb6e45437b231a2bb0327cf3c76
3bdallah99 <83489211+3bdallah99@users.noreply.github.com>
2025-03-11 17:11:08 +0200

    Create create Action , View

--- Company.pro.PL/Controllers/DepartmentController.cs
+++ Company.pro.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.pro.BLL.Interfaces;
 using Company.pro.BLL.Repositories;
+using Company.pro.DAL.Models;
+using Company.pro.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.pro.PL.Controllers
@@ -18,6 +20,31 @@ namespace Company.pro.PL.Controllers
         {
             var depatment = _departmentRepository.GetAll();
             return View(depatment);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server side validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+               var count = _departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
         }
     }
 }
