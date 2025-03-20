2d50eb2edaca2b0c434b7cf17d32ba226e74802f
3bdallah99 <83489211+3bdallah99@users.noreply.github.com>
2025-03-20 22:42:11 +0200

    Finishing Employee Impelementation

--- Company.pro.BLL/Repositories/GenericRepository.cs
+++ Company.pro.BLL/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
-﻿using Company.pro.BLL.Interfaces;
+﻿ using Company.pro.BLL.Interfaces;
 using Company.pro.DAL.Data.Contexts;
 using Company.pro.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,11 +31,19 @@ namespace Company.pro.BLL.Repositories
 
         public T? Get(int Id)
         {
+            if (typeof(T) == typeof(Employee))
+            {
+                return _context.Employees.Include(E => E.Department).FirstOrDefault(E => E.Id == Id) as T;
+            }
             return _context.Set<T>().Find(Id);
         }
 
         public IEnumerable<T> GetAll()
         {
+            if (typeof(T) == typeof(Employee))
+            {
+                return (IEnumerable<T>)_context.Employees.Include(E => E.Department).ToList();
+            }
            return _context.Set<T>().ToList();
         }
 
